3cbf074080fcb6f5f4c18785759e0db5093bcf21
Viktor Forslund <viktor@ekastigen.net>
2025-07-03 17:21:36 +0200

    nu fungerar login

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ var sqlite3 = require("sqlite3");
 var path = require("path");
 var fs = require("fs");
 var util = require("util");
-const { emitWarning } = require("process");
-const { error } = require("console");
 const session = require("express-session");
 
 var app = express();
@@ -19,11 +17,11 @@ var SQL3 = new sqlite3.Database(DB_PATH);
 var data_base = {
   run: util.promisify(SQL3.run.bind(SQL3)),
   get: util.promisify(SQL3.get.bind(SQL3)),
+  exec: util.promisify(SQL3.exec.bind(SQL3)),
 };
 
 var server = http.createServer(app);
 sql_init = fs.readFileSync(SQL_PATH, "utf-8");
-data_base.exec(sql_init);
 
 app.use(express.json());
 app.use(
@@ -34,10 +32,10 @@ app.use(
   })
 );
 
-app.post("/api/post", (req, res) => {
-  var current_count = req.body.count;
-  var username = req.body.username;
-  write_to_data_base(username, current_count);
+app.post("/api/post", async (req, res) => {
+  let current_count = req.body.count;
+  let username = req.body.username;
+  await write_to_data_base(username, current_count);
   res.send("success!");
 });
 
@@ -50,130 +48,102 @@ app.get("/main.html", (req, res, next) => {
 });
 
 app.get("/api/me", (req, res) => {
-  res.json({ username: req.session.username || null });
+  res.json({ username: req.session.username });
 });
 
-app.get("/api/count", (req, res) => {
-  let username = req.query.username;
-  data_base.get(
+app.get("/api/count", async (req, res) => {
+  let username = req.session.username;
+  let result = await data_base.get(
     `
       SELECT count FROM Banana_data WHERE username = ?
     `,
-    username,
-    (err, row) => {
-      data_base.get(
-        `
-        SELECT SUM(count) AS total FROM Banana_data
-        `,
-        (err, child_row) => {
-          if (err) console.log(err);
-          else {
-            if (!row) {
-              data_base.run(
-                `
-            INSERT INTO Banana_data (username, count) VALUES (?, 0)
-            `,
-                username,
-                (err) => {
-                  if (err) {
-                    console.log(err);
-                  } else
-                    res.send({
-                      count: 0,
-                      total_count: child_row ? child_row.total : 0,
-                    });
-                }
-              );
-            } else {
-              res.send({
-                count: row.count,
-                total_count: child_row ? child_row.total : 0,
-              });
-            }
-          }
-        }
-      );
-    }
+    username
   );
+  let sum_result = await data_base.get(
+    `
+        SELECT SUM(count) AS total FROM Banana_data
+        `
+  );
+  res.send({
+    count: result.count,
+    total_count: sum_result.total,
+  });
 });
 
-app.post("/api/login/post", (req, res) => {
+app.post("/api/login/post", async (req, res) => {
   let con = req.query.login === "true";
   let password = req.body.password;
   let username = req.body.username;
   if (con) {
-    check_login(username, password, (exists) => {
-      if (exists) {
-        req.session.username = username;
-        res.json({ success: true, message: "you are logged in!" });
-      } else
-        res.json({
-          success: false,
-          message: "Incorrect username or password! ",
-        });
-    });
+    let result = await check_login(username, password);
+    if (result) {
+      req.session.username = username;
+      res.json({ success: true, message: "you are logged in!" });
+    } else
+      res.json({
+        success: false,
+        message: "Incorrect username or password! ",
+      });
   } else {
-    insert_login(username, password, (success) => {
-      if (!success)
-        res.json({ success: false, message: "This account already exists! " });
-      else res.json({ success: true, message: "Account created! " });
-    });
+    let created = await insert_login(username, password);
+    console.log(created);
+    if (created) {
+      res.json({ success: true, message: "The account has been created! " });
+    } else {
+      res.json({ success: false, message: "The account already exists! " });
+    }
   }
 });
 
 app.use(express.static(WEB_PATH)); //Server my static files (HTML, CSS);
 
-function main() {
+async function main() {
+  await data_base.exec(sql_init);
   server.listen(PORT);
 }
 
-function check_login(username, password, callback) {
-  data_base.get(
-    `
-      SELECT * FROM Banana_data WHERE username = ? AND psw = ?
-    `,
-    [username, password],
-    (err, row) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      if (row) {
-        callback(true);
-      } else {
-        callback(false);
-      }
-    }
-  );
+async function check_login(username, password = undefined) {
+  let result;
+  if (password) {
+    result = await data_base.get(
+      `
+        SELECT * FROM Banana_data WHERE username = ? AND psw = ?
+      `,
+      [username, password]
+    );
+  } else {
+    result = await data_base.get(
+      `
+        SELECT * FROM Banana_data WHERE username = ?
+      `,
+      [username]
+    );
+  }
+  if (result) {
+    return true;
+  }
+  return false;
 }
 
-function insert_login(username, password, callback) {
-  check_login(username, password, (exists) => {
-    if (!exists) {
-      data_base.run(
-        `
-          INSERT INTO Banana_data (username, psw) VALUES (?, ?)
+async function insert_login(username, password) {
+  let exists = await check_login(username);
+  if (!exists) {
+    await data_base.run(
+      `
+          INSERT INTO Banana_data (username, psw, count) VALUES (?, ?, 0)
         `,
-        [username, password],
-        (err) => {
-          if (err) {
-            console.log(err);
-            callback(false);
-          } else callback(true);
-        }
-      );
-    } else callback(false);
-  });
+      [username, password]
+    );
+    return true;
+  }
+  return false;
 }
 
-function write_to_data_base(username, count) {
-  data_base.run(
-    `UPDATE Banana_data SET count = ? WHERE username = ?`,
-    [count, username],
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
+async function write_to_data_base(username, count) {
+  await data_base.run(`UPDATE Banana_data SET count = ? WHERE username = ?`, [
+    count,
+    username,
+  ]);
 }
 
 main();
