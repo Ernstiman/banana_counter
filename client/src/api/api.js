308b197a4e866b9b750a48c5f8d7c6893d6b112b
Viktor Forslund <viktor@ekastigen.net>
2025-07-31 14:25:53 +0200

    test

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -6,7 +6,7 @@ const BASE_URL = import.meta.env.PROD
 : 'http://localhost:4747'
 
 export async function fetch_followers(username) {
-  const followersResponse = await fetch(`${BASE_URL}/api/followers/${username}/followers`, {
+  const followersResponse = await fetch(`api/followers/${username}/followers`, {
     credentials: "include",
   });
   const { userData } = await followersResponse.json();
@@ -14,7 +14,7 @@ export async function fetch_followers(username) {
 }
 
 export async function fetch_following(username) {
-  const followingResponse = await fetch(`${BASE_URL}/api/followers/${username}/following`, {
+  const followingResponse = await fetch(`api/followers/${username}/following`, {
     credentials: "include",
   });
   const { userData } = await followingResponse.json();
@@ -22,7 +22,7 @@ export async function fetch_following(username) {
 };
 
 export async function follow(targetUsername) {
-  await fetch(`${BASE_URL}/api/followers/follow`, {
+  await fetch(`api/followers/follow`, {
     credentials: "include",
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -31,7 +31,7 @@ export async function follow(targetUsername) {
 }
 
 export async function unfollow(targetUsername) {
-  await fetch(`${BASE_URL}/api/followers/unfollow`, {
+  await fetch(`api/followers/unfollow`, {
     credentials: "include",
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
@@ -40,7 +40,7 @@ export async function unfollow(targetUsername) {
 }
 
 export async function fetch_banana_count(users) {
-  const response = await fetch(`${BASE_URL}/api/bananas/get-bananas`, {
+  const response = await fetch(`api/bananas/get-bananas`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -55,7 +55,7 @@ export async function fetch_banana_count(users) {
 
 
 export async function post_banana_count(count) {
-  await fetch(`${BASE_URL}/api/bananas/post-bananas`, {
+  await fetch(`api/bananas/post-bananas`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -66,7 +66,7 @@ export async function post_banana_count(count) {
 }
 
 export async function post_banana_history(amount) {
-  await fetch(`${BASE_URL}/api/banana-history/post-history`, {
+  await fetch(`api/banana-history/post-history`, {
     method: "POST",
     credentials: "include",
     headers: { "Content-Type": "application/json" },
@@ -75,7 +75,7 @@ export async function post_banana_history(amount) {
 }
 
 export async function fetch_banana_history(following) {
-  let response = await fetch(`${BASE_URL}/api/banana-history/get-history`, {
+  let response = await fetch(`api/banana-history/get-history`, {
     method: "POST",
     credentials: "include",
     headers: { "Content-Type": "application/json" },
@@ -90,7 +90,7 @@ export async function post_login(username, password, email, create_account = fal
   let url = create_account ? "/create-account" : "/login"
   
   const req = await fetch(
-    `${BASE_URL}/api/auth${url}`,
+    `api/auth${url}`,
     {
       method: "POST",
       headers: {
@@ -108,7 +108,7 @@ export async function post_login(username, password, email, create_account = fal
 export async function post_friend_requests(sender, receiver) {
 
   await fetch(
-    `${BASE_URL}/api/friend-requests/send-requests`,
+    `api/friend-requests/send-requests`,
     {
       method: "POST",
       headers: {
@@ -121,7 +121,7 @@ export async function post_friend_requests(sender, receiver) {
 
 export async function remove_friend_requests(sender, receiver) {
   await fetch(
-    `${BASE_URL}/api/friend-requests/remove-requests`,
+    `api/friend-requests/remove-requests`,
     {
       method: "DELETE",
       headers: {
@@ -135,14 +135,14 @@ export async function remove_friend_requests(sender, receiver) {
 
 export async function fetch_friend_requests(username) {
   let data = await fetch(
-    `${BASE_URL}/api/friend-requests/get-requests?username=${username}`
+    `api/friend-requests/get-requests?username=${username}`
   );
   let { friend_requests } = await data.json();
   return friend_requests;
 }
 
 export async function fetch_all_users(){
-      const response = await fetch(`${BASE_URL}/api/users/get-users`,
+      const response = await fetch(`api/users/get-users`,
          {credentials: "include"}
       )
       const data = await response.json();
@@ -151,7 +151,7 @@ export async function fetch_all_users(){
    }
 
 export async function putNewPassword(email, newPassword) {
-  const response =await fetch(`${BASE_URL}/api/auth/change-password`, {
+  const response =await fetch(`api/auth/change-password`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -163,7 +163,7 @@ export async function putNewPassword(email, newPassword) {
 }
 
 export async function postResetPasswordLink(email) {
-  await fetch(`${BASE_URL}/api/auth/forgot-password-link`, {
+  await fetch(`api/auth/forgot-password-link`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -173,7 +173,7 @@ export async function postResetPasswordLink(email) {
 }
 
 export async function fetchPasswordReset(token) {
-  const response = await fetch(`${BASE_URL}/api/auth/get-reset-password?token=${token}`, {
+  const response = await fetch(`api/auth/get-reset-password?token=${token}`, {
     method: "GET",
   });
   const {success, email, message} = await response.json();
@@ -181,7 +181,7 @@ export async function fetchPasswordReset(token) {
 }
 
 export async function postNotificationSubscription(subscription, user_id){
-  await fetch(`${BASE_URL}/api/push-notifications/subscribe`, {
+  await fetch(`api/push-notifications/subscribe`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -191,13 +191,13 @@ export async function postNotificationSubscription(subscription, user_id){
 }
 
 export async function getPublicVapidKey(){
-  const res = await fetch(`${BASE_URL}/api/push-notifications/get-public-vapid-key`);
+  const res = await fetch(`api/push-notifications/get-public-vapid-key`);
   const key = await res.json();
   return key
 }
 
 export async function postNotification(username, amount, followers){
-    await fetch(`${BASE_URL}/api/push-notifications/send`, {
+    await fetch(`api/push-notifications/send`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
