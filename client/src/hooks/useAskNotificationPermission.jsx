8e21ac29e57af11fe50c8d4249d0c15f68567b8d
Viktor Forslund <viktor@ekastigen.net>
2025-07-31 17:19:17 +0200

    test

--- client/src/hooks/useAskNotificationPermission.jsx
+++ client/src/hooks/useAskNotificationPermission.jsx
@@ -6,15 +6,30 @@ export default function useAskNotificationPermission(){
     const {username} = useUser()
     async function subscribeUser(){
         const key = await getPublicVapidKey()
+        const converted_key = urlBase64ToUint8Array(key);
         const registration = await navigator.serviceWorker.ready;
 
         const subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey: key
+            applicationServerKey: converted_key
         })  
 
         await postNotificationSubscription(subscription, username)
     }
+
+    function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+
+  return outputArray;
+}
 
     useEffect(()=> {
         if(Notification.permission !== "granted"){
