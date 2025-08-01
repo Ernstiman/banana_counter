20c268e2d581c16347b3652c2aff51022c7493ff
Viktor Forslund <viktor@ekastigen.net>
2025-08-01 15:07:30 +0200

    hej

--- routes/pushNotifications.js
+++ routes/pushNotifications.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const pushNotificationsController = require("../controllers/pushNotificationsController");
 
 router.post("/subscribe", pushNotificationsController.subscribe);
-router.post("/send", pushNotificationsController.send);
+router.post("/send", pushNotificationsController.sendBananaNotification);
 router.get("/get-public-vapid-key", pushNotificationsController.getPublicVapidKey);
 router.post("send-friend-request", pushNotificationsController.sendFriendRequestNotification)
 
