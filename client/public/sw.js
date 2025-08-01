2d3569db525015562c5b8d584090777dd09b9ede
Viktor Forslund <viktor@ekastigen.net>
2025-08-01 18:45:51 +0200

    Hje

--- client/public/sw.js
+++ client/public/sw.js
@@ -2,6 +2,7 @@
 self.addEventListener('push', function(event) {
     console.log('Push event received:', event);
     const data = event.data ? event.data.json() : {};
+    console.log(data.url, "url")
     const title = data.title || 'Banana Pealers!';
     const options = {
         body: data.body || 'You have a new notification.',
