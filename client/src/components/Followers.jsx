34b29b6ec64f2976ec137846229a63d8b0b8f4ef
Viktor <viktor@ekastigen.net>
2025-08-09 00:06:21 +0200

    sorterad följar data

--- client/src/components/Followers.jsx
+++ client/src/components/Followers.jsx
@@ -4,13 +4,23 @@ import { UserNav } from "./Navs";
 import useGetUserFollowers from "../hooks/useGetUserFollowers";
 import { NormalLoader } from "./Loaders";
 import "../style/Components/Followers.css"; 
+import { useEffect } from "react";
 
 
 export default function Followers() {
     const { followers, following } = useUser();
     const { loadingUserFollowers } = useGetUserFollowers();
+    const [sortedFollowers, setSortedFollowers] = useState([]);
+    const [sortedFollowing, setSortedFollowing] = useState([]);
+    const [loadingSorting, setLoadingSorting] = useState(true);
 
-    if (loadingUserFollowers) {
+    useEffect(() => {
+        setSortedFollowers(followers.sort((a, b) => b.count - a.count));
+        setSortedFollowing(following.sort((a, b) => b.count - a.count));
+        setLoadingSorting(false);
+    }, [followers, following]);
+
+    if (loadingUserFollowers || loadingSorting) {
         return (
         <div className="followers-root-container">
             <NormalLoader />
@@ -21,7 +31,7 @@ export default function Followers() {
             <div className="followed-container">
                 <h2>Following</h2>
                 <ul className="followed-list scroll-list">
-                    {following && following.map((user, i) => (
+                    {sortedFollowing && sortedFollowing.map((user, i) => (
                         <li key={i} className="follow-list-item">
                             <UserNav targetUsername={user.username} />
                             <span className="follow-count">🍌 {user.count}</span>
@@ -32,7 +42,7 @@ export default function Followers() {
             <div className="followers-container">
                 <h2>Followers</h2>
                 <ul className="followers-list scroll-list">
-                    {followers && followers.map((user, i) => (
+                    {sortedFollowers && sortedFollowers.map((user, i) => (
                         <li key={i} className="follow-list-item">
                             <UserNav targetUsername={user.username} />
                             <span className="follow-count">🍌 {user.count}</span>
